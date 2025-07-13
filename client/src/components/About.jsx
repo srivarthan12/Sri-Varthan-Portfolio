ee0fc1bc1248087d364d62ee8e2f06c446fbc7a9
srivarthan12 <sri2112045@gmail.com>
2025-07-13 16:53:09 +0530

    Update About.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight, Import } from "lucide-react";
 import ReactLogo from "../assets/react.svg";
 import techyguy from "../assets/techy.png"
 import mern from "../assets/MERN.png"
-import exp from "../assets/Neon Halo Silhouette in Dark__endoftext__.png.png"
+import exp from "../assets/Neon Halo Silhouette in Dark__endoftext__.png"
 
 import "swiper/css";
 import "swiper/css/navigation";
